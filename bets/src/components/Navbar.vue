8349a53fbb69e62c0a884c5a8b3f753f6a2c38bd
BetWithFriend <scab_gel@yahoo.com>
2018-04-10 00:04:48 +0300

    first navbar push

--- bets/src/components/Navbar.vue
+++ bets/src/components/Navbar.vue
@@ -1,13 +1,22 @@
 <template>
   <nav v-if="isLoggedIn">
-      <div class="nav-wrapper green">
+      <div class="nav-wrapper main-navbar">
         <div class="container">
-          <router-link to="/" class="brand-logo">Worldcup 2018 Shutterfly</router-link>    
-          <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
-          <ul class="right hide-on-med-and-down">
-            <li v-if="isLoggedIn"><span class="email black-text">{{currentUser}}</span></li>
-            <li v-if="isAdmin"><router-link to="/leagues">Leagues</router-link></li>
-            <li v-if="isLoggedIn"><button v-on:click="logout" class="btn black">Logout</button></li>
+           
+          <!-- <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a> -->
+          <ul class="left hide-on-med-and-down">
+            <router-link to="/" class="brand-logo left"><li><img src="../assets/logo.png"/></li></router-link>   
+            <li v-if="isLoggedIn">
+              <span class="email black-text dropdown-button" data-activates="dropdown" data-beloworigin="true">
+                {{currentUser}}
+                <i class="material-icons right">arrow_drop_down</i>
+                </span>
+              </li>
+            <li><router-link to="/leagues"><span class="nav-items">STANDINGS</span></router-link></li>
+            <li><router-link to="/leagues"><span class="nav-items">PREDICTIONS</span></router-link></li>
+            <li><router-link to="/leagues"><span class="nav-items">RESULTS</span></router-link></li>
+            <li><router-link to="/leagues"><span class="nav-items">TABLES</span></router-link></li>
+            <!-- <li v-if="isLoggedIn"><button v-on:click="logout" class="btn black">Logout</button></li> -->
           </ul>
           <ul class="side-nav" id="mobile-demo">
             <li v-if="isLoggedIn"><router-link to="/">Dashboard</router-link></li>
@@ -16,30 +25,52 @@
           </ul>
         </div>
       </div>
+    <!-- Dropdown Structure -->
+    <ul id="dropdown" class="dropdown-content collection">
+      <li class="collection-item ">ONE</li>
+      <li class="collection-item ">ONE</li>
+      <li class="collection-item ">ONE</li>
+      <li class="collection-item ">ONE</li>
+    </ul>
     </nav>
   </template>
 
 <script>
 import firebase from 'firebase';
+import db from './firebase/firebaseInit'
+import dbTables from "./firebase/firebaseTables";
+
 export default {
   name: 'navbar',
   
   data() {
     return {
       isLoggedIn: false,
-      currentUser: false,
+      currentUser: "",
       isAdmin: false,
+      currentUserEmail: false
     };
   },
   created() {
     if (firebase.auth().currentUser) {
+      this.currentUserEmail = firebase.auth().currentUser.email;
       this.isLoggedIn = true;
-      this.currentUser = firebase.auth().currentUser.email;
+      db
+        .collection(dbTables.USERS)
+        .where("email", "==", this.currentUserEmail)
+        .get()
+        .then(querySnapshot => {
+          querySnapshot.forEach((doc) => {
+            this.currentUser =  doc.data().name
+            $(".dropdown-button").dropdown();
+          })
+        });
 
       if (this.currentUser == "yanayh90@gmail.com") {
         this.isAdmin = true;
       }
     }
+  
   },
   methods: {
     logout: function() {
@@ -56,6 +87,13 @@ export default {
 
 <style scoped>
 .email {
-  padding-right: 10px;
+  padding: 0 40px;
+}
+
+.main-navbar {
+  background-color:#f4f9ff;
+}
+nav ul a .nav-items {
+   color: #25262b;
 }
 </style>
