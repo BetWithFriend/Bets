799821816d3054bd7b55ac6f59006f1a712b89f9
Ami Hollander <ami.hollander@kycint.com>
2018-04-20 19:31:58 +0300

    init

--- bets/src/components/Tables.vue
+++ bets/src/components/Tables.vue
@@ -147,16 +147,83 @@
 </template>
 
 <script>
+var fetch = require('node-fetch');
+
+
 export default {
   name: "tables",
   data() {
-    return {};
+    tables: []
   },
-  created() {
+  methods: {
+    getTables: function () {
+         return fetch('http://www.skysports.com/world-cup-table');
+    }
+  },
+  mounted: function() {
+    this.getTables()
+    .then(res => res.text())
+    .then(body => {
+        console.log("print body")
+    });
+  },
+  created: function() {
     $(document).ready(function() {
       $(".tabs").tabs();
     });
   }
 };
+
+
+//var cheerio = require('cheerio')
+//var fetch = require('node-fetch');
+
+//{
+//  headers: {
+//     'Access-Control-Allow-Origin': 'http://localhost:8080',
+//     'Access-Control-Allow-Credentials': 'true',
+//     'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
+//     'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token'
+//  }
+//  }
+
+//fetch('http://www.skysports.com/world-cup-table')
+//    .then(res => res.text())
+//    .then(body => {
+//      const $ = cheerio.load(body);
+//
+//      $(".standing-table__table").each(function(i, elm) {
+//        var elm = $(elm)
+//
+//        console.log("title:" + elm.find("caption").text())
+//
+//        console.log("title:" + elm.find("thead").text())
+////        console.log("row:" + elm.find(".standing-table__row"))
+//
+//        elm.find(".standing-table__row").each(function(idx, el) {
+////            if (idx <= 0) return
+//            var el = $(el)
+//
+//            console.log()
+//
+//            var row = {
+//              position: el.find("td:nth-child(1)").text(),
+//              team: el.find("td:nth-child(2)").text().replace(/[\n\t\r]/g,"").trim(),
+//              pl:	el.find("td:nth-child(3)").text(),
+//              w: el.find("td:nth-child(4)").text(),
+//              d: el.find("td:nth-child(5)").text(),
+//              l: el.find("td:nth-child(6)").text(),
+//              f: el.find("td:nth-child(7)").text(),
+//              a: el.find("td:nth-child(8)").text(),
+//              gd:	el.find("td:nth-child(9)").text(),
+//              pts: el.find("td:nth-child(10)").text()
+//            }
+//
+//            console.log(row)
+//        });
+//      });
+//
+//    });
+
 </script>
 
